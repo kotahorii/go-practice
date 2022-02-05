d8a1e266b6ca4402bba78c14bbeef1cc002bcfd8
kotahorii <khorii.micron@gmail.com>
2022-02-05 13:03:01 +0900

    [Update] learn about const

--- variable.go
+++ variable.go
@@ -2,19 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-	var (
-		i    int     = 1
-		f64  float64 = 1.2
-		s    string  = "test"
-		t, f bool    = true, false
-	)
-	fmt.Println(i, f64, s, t, f)
+const Pi = 3.14
 
-	xi := 1
-	xf64 := 1.2
-	xs := "test"
-	xt, xf := true, false
-	fmt.Println(xi, xf64, xs, xt, xf)
-	fmt.Printf("%T\n", xi)
+const (
+	Username = "test_user"
+	Password = "test_pass"
+)
+
+func main() {
+	fmt.Println(Pi, Username, Password)
 }
