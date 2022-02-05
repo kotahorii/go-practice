9f5adf3e57aa7f724cf9f40b2b4409ffaf8c9676
kotahorii <khorii.micron@gmail.com>
2022-02-05 12:58:23 +0900

    [Update] variable

--- variable.go
+++ variable.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	var i int = 1
-	var f64 float64 = 1.2
-	var s string = "test"
-	var t bool = true
-	var f bool = false
+	var (
+		i    int     = 1
+		f64  float64 = 1.2
+		s    string  = "test"
+		t, f bool    = true, false
+	)
 	fmt.Println(i, f64, s, t, f)
+
+	xi := 1
+	xf64 := 1.2
+	xs := "test"
+	xt, xf := true, false
+	fmt.Println(xi, xf64, xs, xt, xf)
+	fmt.Printf("%T\n", xi)
 }
