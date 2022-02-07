3e918440e0be75f6d2d9db1c9647eb8b1cefc172
kotahorii <khorii.micron@gmail.com>
2022-02-07 14:34:21 +0900

    [Update] test.py

--- variable.go
+++ variable.go
@@ -2,13 +2,10 @@ package main
 
 import "fmt"
 
-func foo(params ...int) {
-	fmt.Println(len(params), params)
-}
-
 func main() {
-	s := []int{1, 2, 3}
-	fmt.Println(s)
+	n := 100
+	fmt.Println(&n)
 
-	foo(s...)
+	p := &n
+	fmt.Println(*p)
 }
