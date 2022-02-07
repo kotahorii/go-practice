315e1293b43d9058d5676d40ec69bee72436e6e4
kotahorii <khorii.micron@gmail.com>
2022-02-07 12:44:13 +0900

    [Update] practice

--- variable.go
+++ variable.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"strconv"
 )
 
 func main() {
-	fmt.Println("hello", "world")
-	fmt.Println(string("Hello world"[0]))
-	var s string = "Hello world"
-
-	fmt.Println(strings.Replace(s, "H", "X", 1))
-	fmt.Println(strings.Contains(s, "world"))
+	var s string = "14"
+	i, _ := strconv.Atoi(s)
+	fmt.Printf("%T %v", i, i)
 }
