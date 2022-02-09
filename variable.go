8ee22eb163b0063ab4e9cb5bbf627d30a0ac1c38
kotahorii <khorii.micron@gmail.com>
2022-02-09 11:55:27 +0900

    [Update] test.py

--- variable.go
+++ variable.go
@@ -4,19 +4,13 @@ import "fmt"
 
 type Vertex struct {
 	X, Y int
-	S    string
 }
 
-func changeVertex(v Vertex) {
-	v.X = 100
-}
-
-func changeVertex2(v *Vertex) {
-	v.X = 1000
+func Area(v Vertex) int {
+	return v.X * v.Y
 }
 
 func main() {
-	v := &Vertex{1, 2, "test"}
-	changeVertex2(v)
-	fmt.Println(*v)
+	v := Vertex{3, 4}
+	fmt.Println(Area(v))
 }
