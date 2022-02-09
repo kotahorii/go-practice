77d8a058297736fe140ce6547ea7fb2711c53f48
kotahorii <khorii.micron@gmail.com>
2022-02-09 14:21:19 +0900

    [Update] test.py

--- variable.go
+++ variable.go
@@ -4,29 +4,16 @@ import (
 	"fmt"
 )
 
-func do(i interface{}) {
-	// ii := i.(int)
-	// ii *= 2
-	// fmt.Println(ii)
-	// ss := i.(string)
-	// fmt.Println(ss + "!")
+type Person struct {
+	Name string
+	Age  int
+}
 
-	switch v := i.(type) {
-	case int:
-		fmt.Println(v * 2)
-	case string:
-		fmt.Println(v + "!")
-	default:
-		fmt.Printf("I don't know %T\n", v)
-	}
+func (p Person) String() string {
+	return fmt.Sprintf("My name is %v.", p.Name)
 }
 
 func main() {
-	do(10)
-	do("Mike")
-	do(true)
-
-	var i int = 10
-	ii := float64(10)
-	fmt.Println(i, ii)
+	mike := Person{"Mike", 22}
+	fmt.Println(mike)
 }
