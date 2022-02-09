f71630b5b052937d1097ac02281221943a187f68
kotahorii <khorii.micron@gmail.com>
2022-02-09 14:01:16 +0900

    [Update] test.py

--- variable.go
+++ variable.go
@@ -1,35 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type Human interface {
-	Say() string
-}
+func do(i interface{}) {
+	// ii := i.(int)
+	// ii *= 2
+	// fmt.Println(ii)
+	// ss := i.(string)
+	// fmt.Println(ss + "!")
 
-type Person struct {
-	Name string
-}
-
-type Dog struct {
-	Name string
-}
-
-func (p *Person) Say() string {
-	p.Name = "Mr." + p.Name
-	fmt.Println(p.Name)
-	return p.Name
-}
-
-func DriveCar(human Human) {
-	if human.Say() == "Mr.Mike" {
-		fmt.Println("Run")
-	} else {
-		fmt.Println("Get out")
+	switch v := i.(type) {
+	case int:
+		fmt.Println(v * 2)
+	case string:
+		fmt.Println(v + "!")
+	default:
+		fmt.Printf("I don't know %T\n", v)
 	}
 }
 
 func main() {
-	var mike Human = &Person{"Mike"}
-	var dog Dog = Dog{"dog"}
-	DriveCar(mike)
+	do(10)
+	do("Mike")
+	do(true)
+
+	var i int = 10
+	ii := float64(10)
+	fmt.Println(i, ii)
 }
