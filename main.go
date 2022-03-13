26b7d912f6027b02a0fade21f607c01f629604fa
kotahorii <khorii.micron@gmail.com>
2022-03-13 15:30:16 +0900

    [update] main.go

--- main.go
+++ main.go
@@ -1,17 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"time"
+)
 
 type User struct {
-	Id    int
-	Email string
-}
-
-func (u *User) String() string {
-	return fmt.Sprintf("<%d, %s>", u.Id, u.Email)
+	Id      int
+	Name    string
+	Email   string
+	Created time.Time
 }
 
 func main() {
-	u := &User{Id: 123, Email: "mail@example.com"}
-	fmt.Printf("%s\n", u)
+	src := `
+		{
+			"Id":1,
+			"Name":"Takashi",
+			"Email":"example.com",
+			"Created":"2022-03-13T15:26:11.178205+09:00"
+		}
+		`
+	u := new(User)
+	err := json.Unmarshal([]byte(src), u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%+v\n", u)
 }
