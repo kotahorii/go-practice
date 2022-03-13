5e80eaf742629e327b3bc264c8a392dfb8c135a4
kotahorii <khorii.micron@gmail.com>
2022-03-13 23:35:21 +0900

    [Update] main.go

--- main.go
+++ main.go
@@ -2,28 +2,6 @@ package main
 
 import "fmt"
 
-type Stringify interface {
-	ToString() string
-}
-
-type Person struct {
-	Name string
-	Age  int
-}
-
-type T struct {
-	Id   int
-	Name string
-}
-
-func (t *T) String() string {
-	return fmt.Sprintf("<<%d, %s>>", t.Id, t.Name)
-}
-
-func PrintStr(s fmt.Stringer) {
-	fmt.Println(s.String())
-}
-
 func main() {
-	PrintStr(&T{Id: 3, Name: "Takashi"})
+	fmt.Println("aaaaaa")
 }
